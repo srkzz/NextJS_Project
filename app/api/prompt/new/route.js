e312a4344419f89445fb92c58e94af0829ca4c87
srkz <ruben_alex_23@hotmail.com>
2023-07-11 02:36:34 +0100

    fix typos

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,6 +1,6 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
-import { request } from "express";
+
 
 export const POST = async (request) => {
     const { userId, prompt, tag } = await request.json();
