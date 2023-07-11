781625e30e3f9784012a8b8876c30b6e407788d4
srkz <ruben_alex_23@hotmail.com>
2023-07-11 03:56:56 +0100

    fixed a lot of things

--- models/user.js
+++ models/user.js
@@ -3,13 +3,13 @@ import { Schema, model, models } from 'mongoose';
 const UserSchema = new Schema({
   email: {
     type: String,
-    unique: [true, 'Email already exists!'],
-    required: [true, 'Email is required!'],
+    unique: [true, 'Email já existe!'],
+    required: [true, 'Email é obrigatório!'],
   },
   username: {
     type: String,
-    required: [true, 'Username is required!'],
-    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    required: [true, 'Username é obrigatório!'],
+    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalido! 8-20 caracteres."]
   },
   image: {
     type: String,
